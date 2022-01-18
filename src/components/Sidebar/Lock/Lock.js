fff1168e57505fc3fb18e2bf53b42acee47fbcc4
Aihab Umair <addisonhardley111@gmail.com>
2022-01-18 22:24:17 +0530

    Added sliding lock functionality

--- src/components/Sidebar/Lock/Lock.js
+++ src/components/Sidebar/Lock/Lock.js
@@ -1,21 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, createRef, useEffect } from "react";
 import * as classes from "./Lock.module.css";
 
 const Lock = () => {
   const [showLockModal, setShowLockModal] = useState(true);
   const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
   const [pass, setPass] = useState(["", "", "", ""]);
+  const inputRefArray = [createRef(), createRef(), createRef(), createRef()];
+
+  useEffect(() => {
+    if (showLockModal) {
+      inputRefArray[0]?.current?.focus();
+    }
+  }, [showLockModal]);
 
   const handleUnlock = () => {
-    if (pass.every((el, idx) => parseInt(el) === parseInt(process.env.REACT_APP_PIN[idx]))) {
-      setShowLockModal(false);
+    if (
+      pass.every(
+        (el, idx) => parseInt(el) === parseInt(process.env.REACT_APP_PIN[idx])
+      )
+    ) {
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowLockModal(false);
+        setShowSuccess(false);
+      }, 300);
     } else {
+      inputRefArray[0]?.current?.focus();
       setShowError(true);
       setTimeout(() => {
         setShowError(false);
       }, 1000);
     }
     setPass(["", "", "", ""]);
+  };
+
+  const focusInput = (idx) => {
+    inputRefArray[idx]?.current?.focus();
+  };
+
+  const handleBackspace = (idx) => {
+    if (pass[idx] === "") {
+      focusInput(idx - 1);
+    } else {
+      pass[idx] = "";
+    }
   };
 
   const setPassUtil = (event, idx) => {
@@ -28,6 +57,11 @@ const Lock = () => {
           : p
       )
     );
+    if (!isNaN(event.target.value)) {
+      if (event.target.value !== "" && idx !== 3) {
+        focusInput(idx + 1);
+      }
+    }
   };
 
   const lockModal = (
@@ -37,49 +71,69 @@ const Lock = () => {
           <input
             value={pass[0]}
             onChange={(e) => setPassUtil(e, 0)}
-            className={`${classes.input} ${showError && classes.error}`}
+            className={`${classes.input} ${showError && classes.error} ${
+              showSuccess && classes.success
+            }`}
             onKeyDown={(e) => {
-              if(e.key === "Enter") {
+              if (e.key === "Enter") {
                 handleUnlock();
               }
             }}
-          ></input>
+            ref={inputRefArray[0]}
+          />
           <input
             value={pass[1]}
             onChange={(e) => setPassUtil(e, 1)}
-            className={`${classes.input} ${showError && classes.error}`}
+            className={`${classes.input} ${showError && classes.error} ${
+              showSuccess && classes.success
+            }`}
             onKeyDown={(e) => {
-              if(e.key === "Enter") {
+              if (e.key === "Enter") {
                 handleUnlock();
+              } else if (e.key === "Backspace") {
+                handleBackspace(1);
               }
             }}
-          ></input>
+            ref={inputRefArray[1]}
+          />
           <input
             value={pass[2]}
             onChange={(e) => setPassUtil(e, 2)}
-            className={`${classes.input} ${showError && classes.error}`}
+            className={`${classes.input} ${showError && classes.error} ${
+              showSuccess && classes.success
+            }`}
             onKeyDown={(e) => {
-              if(e.key === "Enter") {
+              if (e.key === "Enter") {
                 handleUnlock();
+              } else if (e.key === "Backspace") {
+                handleBackspace(2);
               }
             }}
-          ></input>
+            ref={inputRefArray[2]}
+          />
           <input
             value={pass[3]}
             onChange={(e) => setPassUtil(e, 3)}
-            className={`${classes.input} ${showError && classes.error}`}
+            className={`${classes.input} ${showError && classes.error} ${
+              showSuccess && classes.success
+            }`}
             onKeyDown={(e) => {
-              if(e.key === "Enter") {
+              if (e.key === "Enter") {
                 handleUnlock();
+              } else if (e.key === "Backspace") {
+                handleBackspace(3);
               }
             }}
-          ></input>
+            ref={inputRefArray[3]}
+          />
         </div>
         <button
           onClick={handleUnlock}
-          className={`${classes.unlockButton} ${showError && classes.error}`}
+          className={`${classes.unlockButton} ${showError && classes.error} ${
+            showSuccess && classes.success
+          }`}
         >
-          {showError ? "Incorrect PIN" : "Unlock"}
+          {showError ? "Incorrect PIN" : showSuccess ? "Correct PIN" : "Unlock"}
         </button>
       </div>
     </div>
